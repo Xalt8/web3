c13ee8869edbe69eb5606fbcd44e79fc60575b99
Karan Singh <singh_karan@hotmail.com>
2022-05-14 16:35:27 +0530

    Changeing the event handler

--- guess_number_app_anvil_server.py
+++ guess_number_app_anvil_server.py
@@ -1,26 +1,40 @@
 import anvil.server
 from hexbytes import HexBytes
-from guess_number_app_backend import web3_connection, contract_address, contract
+from guess_number_app_backend import web3_connection, get_abi_byteCode
 from web3 import Web3
 
-# Connect to Anvil server using uplink key
 anvil.server.connect('server_QU6WQ53GJBCZ7MY7GOGX7YRZ-TU5EUJ7KKAH53HP3')
 
-@anvil.server.callable
-def get_contract_address():
-    global contract_address
-    return contract_address
+abi, _ = get_abi_byteCode('Vyper_01/build/contracts/GuessNumberApp.json')
+contract = web3_connection.eth.contract(address='0x2Ca1Ccdb67FFfe598d012818bB6f68fE6AE708C0', abi=abi)
+
 
 @anvil.server.callable
 def get_game_balance() -> int:
     global contract
     return contract.functions.game_balance().call()
 
+
+@anvil.server.callable
+def get_guess_count() -> int:
+    global contract 
+    return contract.functions.guess_count().call()
+
+
+@anvil.server.callable
+def get_players_address():
+    global contract
+    return contract.functions.game_player().call()
+
+
 @anvil.server.callable
 def get_nonce(ETH_address):
+    global web3_connection
     ''' Get the nonce of an Eth address -> used in transfer_eth()'''
     return web3_connection.eth.get_transaction_count(ETH_address)
 
+
+# Not used in the app
 @anvil.server.callable
 def transfer_eth(sender, signature):
     ''' Use this function to transfer 1 Eth from player's account to default account (game creator)'''
@@ -38,25 +52,34 @@ def transfer_eth(sender, signature):
     return string_result
 
 
-# Set the node provider to Ganache
-NODE_PROVIDER = 'http://127.0.0.1:7545'
-web3_connection = Web3(Web3.HTTPProvider(NODE_PROVIDER))
+@anvil.server.callable
+def player_registration(player_address, signature):
+    global web3_connection, contract
+    transaction_body = {
+        'nonce': get_nonce(player_address),
+        'value': web3_connection.toWei(1, 'ether'),
+        'gas': 4500000,
+        'gasPrice': web3_connection.toWei(8, 'gwei')
+        }
+    
+    function_call = contract.functions.player_registration().buildTransaction(transaction_body)
+    signed_transaction = web3_connection.eth.account.sign_transaction(function_call, signature)
+    tx_hash = web3_connection.eth.send_raw_transaction(signed_transaction.rawTransaction)
+    tx_receipt = web3_connection.eth.wait_for_transaction_receipt(tx_hash) 
+    return tx_receipt.status
 
-set_default_account(0)
-abi, byte_code = get_abi_byteCode('Vyper_01/build/contracts/GuessNumberApp.json')
-contract_app = instantiate_contract(abi=abi, byte_code=byte_code)
-tx_hash = deploy_contract_get_tx_hash(secret_number=8, value=10)
-tx_receipt = get_tx_receipt(tx_hash)
-contract_address = get_contract_address(tx_receipt)
-contract = get_contract_instance(contract_address=contract_address, abi=abi)
-player_registration_event = contract.events.Player_registered()
 
+@anvil.server.callable
+def play(guessed_number, player_address):
+    global contract
+    return contract.functions.play(guessed_number).call({'from':player_address})
+    
 
 
 if __name__ == '__main__':
     print("\nConnected to Ganache" if web3_connection.isConnected() else "Not connected to Ganache")
-    print(f'\nContract address:{contract_address}')
 
+    
     anvil.server.wait_forever()
 
     
